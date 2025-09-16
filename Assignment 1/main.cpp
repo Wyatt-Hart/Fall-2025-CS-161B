8f059c37713888a414a71be531b7b702c8f77ea6
Wyatt-Hart <wyatt.c.hart@gmail.com>
2025-09-16 07:56:21 -0500

    Changed git remote from HTTPS to SSH: Testing commits work

--- Assignment 1/main.cpp
+++ Assignment 1/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main() {
 	cout << "Hello, World!" << endl;
 	cout << "I have successfully committed my changes!" << endl;
+	cout << "OK, I should be good to go now!" << endl;
 
 	return 0;
 }
